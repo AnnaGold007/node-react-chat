1f93ea4ada65342cc9e7c09e59d005cec86cf765
Anna Gold <38617553+AnnaGold007@users.noreply.github.com>
2022-02-10 09:45:03 +0200

    ChatAppUpload

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-react-chat
+ Chat Application build using Node, React, Expree and Socket.IO
---
+++ client/build/precache-manifest.41cd4d01f3f43cec416cbb89e78ef647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1679f96fb4fc5a73c61c8be943a7d212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bfe257268b0682711e6",
+    "url": "/static/css/main.a1892929.chunk.css"
+  },
+  {
+    "revision": "5a8d74e5a78ebf9a6389",
+    "url": "/static/js/2.e5297b84.chunk.js"
+  },
+  {
+    "revision": "2bfe257268b0682711e6",
+    "url": "/static/js/main.944fad4a.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
